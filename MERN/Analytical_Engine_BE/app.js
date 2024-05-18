a5895d9e9f6658a92fb8380d329cd5c20f212bf6
Dhyaneshwar N <dhyaneshwar98@gmail.com>
2024-05-18 17:13:39 +0100

    feat: Mongo DB connect is established

--- MERN/Analytical_Engine_BE/app.js
+++ MERN/Analytical_Engine_BE/app.js
@@ -33,3 +33,15 @@ app.use((error, req, res, next) => {
     .status(error.code || 500)
     .json({ message: error.message || "An unknown error occurred!" });
 });
+
+const { DB_USERNAME, DB_PASSWORD, DB_CLUSTER, DB_NAME } = process.env;
+
+mongoose
+  .connect(
+    `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_CLUSTER}/${DB_NAME}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    PORT = process.env.PORT || 5000;
+    app.listen(PORT);
+  })
+  .catch((err) => console.log(err));
